f0d0ca22094b2df283ee35c7e108b7bdb9ae0cdc
devmrwor <michaelryanrm18@gmail.com>
2023-07-26 15:12:47 +0100

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,12 @@ require 'faker'
 end
 
 100.times do
-  Post.create(
+  post = Post.create(
     title: Faker::Lorem.sentence(word_count: 3),
-    content: Faker::Lorem.paragraph(sentence_count: 2)
+    content: Faker::Lorem.paragraph(sentence_count: 2),
+    label: Faker::Lorem.word,
   )
+  post.update_attribute(:created_at, Faker::Date.between(from: 365.days.ago, to: Date.today))
+  p "Created post - title: #{post.title} | content: #{post.content.truncate(20)} | label: #{post.label} | created_at: #{post.created_at}"
 end
+p "Created #{Post.count} posts"
