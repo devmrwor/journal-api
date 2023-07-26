d87373fcb3de11def95c03af0a4c15b6394edb78
devmrwor <michaelryanrm18@gmail.com>
2023-07-26 15:36:38 +0100

    update cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com"
+    origins "http://localhost:4000"
 
     resource "*",
       headers: :any,
