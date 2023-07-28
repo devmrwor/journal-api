7235c43ead96b930a3d80d382d4f83b320368e21
devmrwor <michaelryanrm18@gmail.com>
2023-07-28 21:46:53 +0100

    add post model validations and tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is valid with valid attributes" do
+      post = build(:post)
+      expect(post).to be_valid
+    end
+
+    it "is not valid without a title" do
+      post = build(:post, title: nil)
+      expect(post).not_to be_valid
+      expect(post.errors[:title]).to include("can't be blank")
+    end
+
+    it "is not valid without content" do
+      post = build(:post, content: nil)
+      expect(post).not_to be_valid
+      expect(post.errors[:content]).to include("can't be blank")
+    end
+  end
 end
