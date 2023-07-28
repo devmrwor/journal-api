7e5a98401e4be196cd28b435f0e141bf90448cd8
devmrwor <michaelryanrm18@gmail.com>
2023-07-28 09:14:08 +0100

    change seed created date to be one per day

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,16 @@ require 'faker'
   ActiveRecord::Base.connection.execute("TRUNCATE #{table.table_name} RESTART IDENTITY CASCADE")
 end
 
+day_count = 0
+
 100.times do
   post = Post.create(
     title: Faker::FunnyName.name,
     content: Faker::Quote.famous_last_words,
     label: ["idea", "fun", "work", "life"].sample,
   )
-  post.update_attribute(:created_at, Faker::Date.between(from: 365.days.ago, to: Date.today))
+  post.update_attribute(:created_at, Date.today - day_count)
   p "Created post - title: #{post.title} | content: #{post.content.truncate(20)} | label: #{post.label} | created_at: #{post.created_at}"
+  day_count += 1
 end
 p "Created #{Post.count} posts"
