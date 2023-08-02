2ca91e90b74b33bab92e65e71c155d8ebf58d5e5
devmrwor <michaelryanrm18@gmail.com>
2023-08-02 22:09:56 +0100

    fix: typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4000", "https://journal-frontend-smoky.vercel.app/"
+    origins "http://localhost:4000", "https://journal-frontend-smoky.vercel.app"
 
     resource "*",
       headers: :any,
