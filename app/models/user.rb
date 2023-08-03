87368de462b494d5479c006ec8b3c9446f135087
devmrwor <michaelryanrm18@gmail.com>
2023-08-03 14:25:59 +0100

    update user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :username, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with valid attributes" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+  it "is not valid without an email" do
+    user = build(:user, email: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
+  it "is not valid without a password" do
+    user = build(:user, password: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:password]).to include("can't be blank")
+  end
 end
